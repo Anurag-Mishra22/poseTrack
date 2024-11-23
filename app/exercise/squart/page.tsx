1783b5d98188405752b41b1bbc66f1db1085e18b
Anurag-Mishra22 <anurag2025mishra@gmail.com>
2024-11-23 17:21:16 +0530

    score changed

--- app/exercise/squart/page.tsx
+++ app/exercise/squart/page.tsx
@@ -108,12 +108,12 @@ export default function Home() {
         const rightAnkle = keypoints.find((kp: any) => kp.name === "right_ankle");
 
         if (
-            leftHip?.score > 0.5 &&
-            leftKnee?.score > 0.5 &&
-            leftAnkle?.score > 0.5 &&
-            rightHip?.score > 0.5 &&
-            rightKnee?.score > 0.5 &&
-            rightAnkle?.score > 0.5
+            leftHip?.score > 0.7 &&
+            leftKnee?.score > 0.7 &&
+            leftAnkle?.score > 0.7 &&
+            rightHip?.score > 0.7 &&
+            rightKnee?.score > 0.7 &&
+            rightAnkle?.score > 0.7
         ) {
 
             if (leftHip && leftKnee && leftAnkle && rightHip && rightKnee && rightAnkle) {
@@ -155,12 +155,12 @@ export default function Home() {
         console.log("Keypoints:", leftHip, leftKnee, leftAnkle, rightHip, rightKnee, rightAnkle);
 
         if (
-            leftHip?.score > 0.5 &&
-            leftKnee?.score > 0.5 &&
-            leftAnkle?.score > 0.5 &&
-            rightHip?.score > 0.5 &&
-            rightKnee?.score > 0.5 &&
-            rightAnkle?.score > 0.5
+            leftHip?.score > 0.7 &&
+            leftKnee?.score > 0.7 &&
+            leftAnkle?.score > 0.7 &&
+            rightHip?.score > 0.7 &&
+            rightKnee?.score > 0.7 &&
+            rightAnkle?.score > 0.7
         ) {
 
             // Ensure all keypoints are valid before drawing
